45461464b0208275b27b5f2568f6e00b9665738b
Eneldo Serrata <eneldoserrata@gmail.com>
2018-04-26 18:21:02 -0400

    update clean-css

--- openerp/addons/base/ir/ir_qweb.py
+++ openerp/addons/base/ir/ir_qweb.py
@@ -1631,7 +1631,7 @@ class LessStylesheetAsset(PreprocessedCSS):
             lessc = 'lessc'
         webpath = openerp.http.addons_manifest['web']['addons_path']
         lesspath = os.path.join(webpath, 'web', 'static', 'lib', 'bootstrap', 'less')
-        return [lessc, '-', '--clean-css', '--js', '--no-color', '--include-path=%s' % lesspath]
+        return [lessc, '-', '--less-plugin-clean-css.clean-css', '--js', '--no-color', '--include-path=%s' % lesspath]
         # return [lessc, '-', '--clean-css', '--no-js', '--no-color', '--include-path=%s' % lesspath]
 
 def rjsmin(script):
